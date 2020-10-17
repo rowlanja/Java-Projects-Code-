b3c56333331dfc2b93cf968a8d1055da53cba82d
rowlanja <rowlanja@tcd.ie>
2020-10-17 19:22:45 +0100

    updated tests

--- Java_LowestCommonAncestor/LCA/src/LCA.java
+++ Java_LowestCommonAncestor/LCA/src/LCA.java
@@ -2,37 +2,44 @@ import java.util.*;
 public class LCA {	
 
 
+	@SuppressWarnings("finally")
 	public String findAnchestors(HashMap<String,ArrayList<String>> relations, String person1, String person2) {
 		String returned  = "", person1Ancest = "", person2Ancest = "";
 		Boolean found = false;
-		while(!found) {
-			boolean parents = false;
-			for (Map.Entry<String, ArrayList<String>> entry : relations.entrySet()) {
-			    String key = entry.getKey();
-			    ArrayList<String> value = entry.getValue();
-			    //if the current Person is the parent of the two children in question
-			    if(value.contains(person1) && value.contains(person2)) {
-					returned =  key;
-					found = true;
+		try {
+			while(!found) {
+				boolean parents = false;
+				for (Map.Entry<String, ArrayList<String>> entry : relations.entrySet()) {
+					String key = entry.getKey();
+					ArrayList<String> value = entry.getValue();
+					//if the current Person is the parent of the two children in question
+					if(value.contains(person1) && value.contains(person2)) {
+						returned =  key;
+						found = true;
+					}
+					//if the current Person is the parent of child 1
+					if(value.contains(person1)) {
+						person1Ancest += key;
+						person1 = key;
+						parents = true;
+						if(person2Ancest.contains(key)) return key;
+					}
+					//if the current Person is the parent of child 2
+					if(value.contains(person2)) {
+						person2Ancest += key;
+						person2 = key;
+						parents = true;
+						if(person1Ancest.contains(key)) return key;	
+					}
 				}
-			    //if the current Person is the parent of child 1
-			    if(value.contains(person1)) {
-					person1Ancest += key;
-					person1 = key;
-					parents = true;
-					if(person2Ancest.contains(key)) return key;
-				}
-			    //if the current Person is the parent of child 2
-			    if(value.contains(person2)) {
-					person2Ancest += key;
-					person2 = key;
-					parents = true;
-					if(person1Ancest.contains(key)) return key;	
-				}
+				if(parents == false) break;
 			}
-			if(parents == false) break;
+		} catch(Exception E) {
+			System.out.println("Exception occured : " + E);
 		}
-		return returned;
+		finally {
+			return returned;
+		}
 	}
 
 	public static void main(String[] args) {
--- Java_LowestCommonAncestor/LCA/src/LCATest.java
+++ Java_LowestCommonAncestor/LCA/src/LCATest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 
 class LCATest {
 
+	
+	// MEDIUM SIZED FAMILY TREE
 	@Test
 	void test1() {
 		LCA test1 = new LCA();
@@ -26,7 +28,7 @@ class LCATest {
 		System.out.println("common ancestor : " + x);
 		assertEquals(x, "C", "Common Ancestor must be C");
 	}
-	
+	// TEST OF SMALL FAMILY TREE
 	@Test
 	void test2() {
 		LCA test1 = new LCA();
@@ -41,7 +43,7 @@ class LCATest {
 
 		assertEquals(y, "A", "Common Ancestor must be A");
 	}
-	
+	// TEST WITH NO RELATIONS
 	@Test
 	void test3() {
 		LCA test1 = new LCA();
@@ -57,4 +59,22 @@ class LCATest {
 	}
 
 	
+	// LARGE FAMILY TREE
+	@Test
+	void test4() {
+		LCA test1 = new LCA();
+		HashMap<String,ArrayList<String>> relations2 =
+				new HashMap<String,ArrayList<String>>();//Creating HashMap
+		
+		relations2.put("A", new ArrayList<String>(Arrays.asList("B", "E")));
+		relations2.put("B", new ArrayList<String>(Arrays.asList("C")));
+		relations2.put("C", new ArrayList<String>(Arrays.asList("D")));
+		relations2.put("D", new ArrayList<String>());
+		relations2.put("E", new ArrayList<String>(Arrays.asList("F")));
+		relations2.put("F", new ArrayList<String>(Arrays.asList("G")));
+		relations2.put("G", new ArrayList<String>());
+		String y = test1.findAnchestors(relations2, "D", "G");
+		System.out.println("common ancestor : " + y);
+		assertEquals(y, "A", "Common Ancestor must be A");
+	}
 }
